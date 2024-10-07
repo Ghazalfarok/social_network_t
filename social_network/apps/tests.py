9bc1e12be95d27b9709b8e071b6639984ba5af98
itsMajid <itsMajid.dev@gmail.com>
2024-10-07 15:49:55 +0330

    'apps'  app development and test writing for 'apps'

--- social_network/apps/tests.py
+++ social_network/apps/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from django.shortcuts import reverse
+import time 
 # Create your tests here.
+class AppsTest(TestCase):
+    def test_init_url(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code , 200)
+
+    def test_init_name(self):
+        response = self.client.get(reverse("Home page"))
+        self.assertEqual(response.status_code , 200)
+
+    def test_time_load(self):
+        start_load = time.time()
+        response = self.client.get(reverse("Home page"))
+        end_load = time.time()
+        time_load = end_load - start_load
+        self.assertLess(time_load , 1)
