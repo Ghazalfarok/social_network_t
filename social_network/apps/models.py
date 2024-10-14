3c67cf66e6cdcf42368474c309f1ff2207ba57cf
itsMajid <itsMajid.dev@gmail.com>
2024-10-14 22:01:58 +0330

    create User model

--- social_network/apps/models.py
+++ social_network/apps/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=51)
+    password = models.CharField(max_length=51)
+    phone = models.CharField(max_length=11)
+
+
+    def __str__(self):
+        return f"{self.username}"
